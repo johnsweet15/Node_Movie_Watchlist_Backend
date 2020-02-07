8245ef44596d2eaa1b27abb92f6d5ab762bf3722
johnsweet15 <john.sweet.15@cnu.edu>
2020-02-07 03:30:13 -0500

    added get user profile endpoint

--- routes/auth.js
+++ routes/auth.js
@@ -5,7 +5,9 @@ const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
 const { registerValidation, loginValidation } = require('../validation')
 
-
+/*
+  REGISTER USER
+*/
 router.post('/register', async (req, res) => {
 
   // Validate data
@@ -35,35 +37,57 @@ router.post('/register', async (req, res) => {
     const savedUser = await user.save()
     res.send({ user: user._id })
   } catch (err) {
-    res.status(400).send(err)
+    res.status(400).send({ message: err })
   }
 })
 
-// Login
-router.post('/login', async(req, res) => {
+/*
+  LOGIN USER
+*/
+router.post('/login', async (req, res) => {
 
   // Validate data
   const { error } = loginValidation(req.body)
   if (error) {
-    return res.status(400).send({success: false, message: error.details[0].message})
+    return res.status(400).send({ success: false, message: error.details[0].message })
   }
 
   // Check if user is already in database
   const user = await User.findOne({ email: req.body.email })
   if (!user) {
-    return res.status(403).send({success: false, message: 'Email or password incorrect'})
+    return res.status(403).send({ success: false, message: 'Email or password incorrect' })
   }
 
   // Check if password is correct
   const validPassword = await bcrypt.compare(req.body.password, user.password)
-  if(!validPassword) {
-    return res.status(403).send({success: false, message: 'Password incorrect'})
+  if (!validPassword) {
+    return res.status(403).send({ success: false, message: 'Password incorrect' })
   }
 
   // Create and assign token
-  const token = jwt.sign({_id: user._id}, process.env.TOKEN_SECRET)
-  res.header('auth-token', token).send({success: true, token: token, _id: user._id})
+  const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET)
+  res.header('auth-token', token).send({ success: true, token: token, _id: user._id })
 
+})
+
+/*
+  GET USER PROFILE
+*/
+router.get('/:userId', async (req, res) => {
+  try {
+    const user = await User.findOne({
+      _id: req.params.userId
+    })
+    res.status(200).send({
+      profile: {
+        name: user.name,
+        email: user.email,
+        date: user.date
+      }
+    })
+  } catch (error) {
+    res.status(400).send({ message: error })
+  }
 })
 
 module.exports = router
